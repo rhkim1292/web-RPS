d0b1b21f251de4700c4c06efa6f55cc6ab50f97b
Randy Kim <rhkim1292@gmail.com>
2022-06-22 00:41:11 -0700

    Implement the playRound function in rps.js
    The playRound function takes in the user input string as the first
    parameter and the computer's random string as the second parameter.
    The function uses those two input values to determine what message
    to return to the user.

--- rps.js
+++ rps.js
@@ -9,4 +9,21 @@ function computerPlay() {
     }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelectionLowerCase = playerSelection.toLowerCase();
+    if (playerSelectionLowerCase === 'rock') {
+        if (computerSelection === 'Paper') return 'You Lose! Paper beats Rock';
+        if (computerSelection === 'Rock') return 'It\'s a tie!';
+        if (computerSelection === 'Scissors') return 'You Win! Rock beats Scissors!';
+    } else if (playerSelectionLowerCase === 'paper') {
+        if (computerSelection === 'Paper') return 'It\'s a tie!';
+        if (computerSelection === 'Rock') return 'You Win! Paper beats Rock!';
+        if (computerSelection === 'Scissors') return 'You Lose! Scissors beats Paper';
+    } else if (playerSelectionLowerCase === 'scissors') {
+        if (computerSelection === 'Paper') return 'You Win! Scissors beats Paper!';
+        if (computerSelection === 'Rock') return 'You Lose! Rock beats Scissors!';
+        if (computerSelection === 'Scissors') return 'It\'s a tie!';
+    }
+}
+
+console.log(playRound('paper', 'Scissors'));
