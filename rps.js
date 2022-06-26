25c06ab3df4089f307da1af864976459f386e1b9
Randy Kim <rhkim1292@gmail.com>
2022-06-26 02:26:48 -0700

    Add a UI to the html with styling
    This commit also makes the "Rock" button functioning by altering the
    player/cpu win count display in the results card on the UI whenever it's
    pressed.

--- rps.js
+++ rps.js
@@ -1,31 +1,62 @@
 // Randomly determines the cpu player's choice between rock, paper, or scissors.
 function computerPlay() {
     let roll = Math.random();
-    if (roll < .33) {
-        return 'Rock';
-    } else if (roll >= .33 && roll < .67) {
-        return 'Paper';
+    if (roll < 0.33) {
+        return "Rock";
+    } else if (roll >= 0.33 && roll < 0.67) {
+        return "Paper";
     } else {
-        return 'Scissors';
+        return "Scissors";
     }
 }
 
 // Plays one round of RPS using the player's choice and the cpu's choice.
 function playRound(playerSelection, computerSelection) {
     playerSelectionLowerCase = playerSelection.toLowerCase();
-    if (playerSelectionLowerCase === 'rock') {
-        if (computerSelection === 'Paper') return 'You Lose! Paper beats Rock';
-        if (computerSelection === 'Rock') return 'It\'s a tie!';
-        if (computerSelection === 'Scissors') return 'You Win! Rock beats Scissors!';
-    } else if (playerSelectionLowerCase === 'paper') {
-        if (computerSelection === 'Paper') return 'It\'s a tie!';
-        if (computerSelection === 'Rock') return 'You Win! Paper beats Rock!';
-        if (computerSelection === 'Scissors') return 'You Lose! Scissors beats Paper';
-    } else if (playerSelectionLowerCase === 'scissors') {
-        if (computerSelection === 'Paper') return 'You Win! Scissors beats Paper!';
-        if (computerSelection === 'Rock') return 'You Lose! Rock beats Scissors!';
-        if (computerSelection === 'Scissors') return 'It\'s a tie!';
+    if (playerSelectionLowerCase === "rock") {
+        if (computerSelection === "Paper") {
+            return "You Lose! Paper beats Rock";
+        }
+        if (computerSelection === "Rock") {
+            return "It's a tie!";
+        }
+        if (computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors!";
+        }
+    } else if (playerSelectionLowerCase === "paper") {
+        if (computerSelection === "Paper") {
+            return "It's a tie!";
+        }
+        if (computerSelection === "Rock") {
+            return "You Win! Paper beats Rock!";
+        }
+        if (computerSelection === "Scissors") {
+            return "You Lose! Scissors beats Paper";
+        }
+    } else if (playerSelectionLowerCase === "scissors") {
+        if (computerSelection === "Paper") {
+            return "You Win! Scissors beats Paper!";
+        }
+        if (computerSelection === "Rock") {
+            return "You Lose! Rock beats Scissors!";
+        }
+        if (computerSelection === "Scissors") {
+            return "It's a tie!";
+        }
     }
+}
+
+// Determines the winner of the round
+function determineWinner(results) {
+    let resultsSubstr = results.substring(0, 5);
+
+    if (resultsSubstr === "You W") {
+        return 'player';
+    } else if (resultsSubstr === 'You L') {
+        return 'cpu';
+    }
+
+    return 'tie';
 }
 
 // Executes the entire game of RPS
@@ -33,25 +64,41 @@ function game() {
     let playerWins = 0;
     let cpuWins = 0;
 
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt('It\'s time to choose! Rock, Paper, or Scissors?!');
-        let results = playRound(playerChoice, computerPlay());
-        console.log(results);
-
-        results = results.substring(0, 5);
-
-        if (results === 'You W') {
+    const rockBtn = document.querySelector("#rockBtn");
+    const paperBtn = document.querySelector("#paperBtn");
+    const scissorsBtn = document.querySelector("#scissorsBtn");
+    const winsDisplayContainer = document.querySelector(".wins-display-container");
+    const playerWinsElement = document.querySelector("#playerWins");
+    const cpuWinsElement = document.querySelector("#cpuWins");
+    const resultTextContainer = document.querySelector(".result-text-container");
+    const resultText = document.querySelector("#resultText");
+    
+    rockBtn.addEventListener("click", () => {
+        resultText.textContent = playRound("rock", computerPlay());
+        let winnerOfRound = determineWinner(resultText.textContent);
+        if (winnerOfRound === 'player') {
             playerWins++;
-        } else if (results === 'You L') {
+        } else if (winnerOfRound === 'cpu') {
             cpuWins++;
         }
-    }
+        playerWinsElement.textContent = `Your Wins: ${playerWins}`;
+        cpuWinsElement.textContent = `CPU Wins: ${cpuWins}`;
+        winsDisplayContainer.replaceChildren(playerWinsElement, cpuWinsElement);
+    });
 
-    console.log('Here are the final results...\nPlayer Wins: ' + playerWins + '\nCPU Wins: ' + cpuWins);
+    console.log(
+        "Here are the final results...\nPlayer Wins: " +
+            playerWins +
+            "\nCPU Wins: " +
+            cpuWins
+    );
+
     if (playerWins > cpuWins) {
-        console.log('The player wins overall with ' + playerWins + ' round wins!');
+        console.log(
+            "The player wins overall with " + playerWins + " round wins!"
+        );
     } else {
-        console.log('The cpu wins overall with ' + cpuWins + ' round wins!');
+        console.log("The cpu wins overall with " + cpuWins + " round wins!");
     }
 }
 
